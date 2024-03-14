6d93c0658eb830447bd9b1edb7e36a7d917f2d51
Andy Blight <a.j.blight@leeds.ac.uk>
2024-03-14 16:58:56 +0000

    Allow optional building for testing.
    Added two #defines so the publisher and subscriber can be built
    independently or together.

--- examples/micro-ros_subscriber/src/Main.cpp
+++ examples/micro-ros_subscriber/src/Main.cpp
@@ -6,25 +6,30 @@
 #include <rclc/executor.h>
 
 #include <geometry_msgs/msg/twist.h>
+#include <std_msgs/msg/int32.h>
 
-#define SSID "YOUR_SSID"
-#define SSID_PW "your_password"
+#define ENABLE_SUBSCRIBER (1)
+#define ENABLE_PUBLISHER (1)
 
-// #if !defined(MICRO_ROS_TRANSPORT_ARDUINO_SERIAL)
-// #error This example is only avaliable for Arduino framework with serial transport.
-// #endif
+#define SSID "your ssid"
+#define SSID_PW "your password"
 
-static const char *TAG = "main";
+#if ENABLE_PUBLISHER
+rcl_publisher_t publisher;
+std_msgs__msg__Int32 msg;
+#endif
 
+#if ENABLE_SUBSCRIBER
 static const char *k_twist = "cmd_vel";
 static rcl_subscription_t subscriber_twist;
 static geometry_msgs__msg__Twist *twist_msg;
+#endif
 
 rclc_executor_t executor;
 rclc_support_t support;
 rcl_allocator_t allocator;
 rcl_node_t node;
-// rcl_timer_t timer;
+rcl_timer_t timer;
 
 #define RCCHECK(fn)                                                    \
   {                                                                    \
@@ -32,7 +37,6 @@ rcl_node_t node;
     if ((temp_rc != RCL_RET_OK)) {                                     \
       printf("Failed status on line %d: %d. Message: %s, Aborting.\n", \
              __LINE__, (int)temp_rc, rcl_get_error_string().str);      \
-      rcutils_reset_error();                                           \
       error_loop(temp_rc);                                             \
     }                                                                  \
   }
@@ -49,26 +53,45 @@ rcl_node_t node;
 
 // Error handle loop
 void error_loop(rcl_ret_t rc) {
-  ESP_LOGE(TAG, "Entering Error Loop with rc = %d", rc);
+  Serial.println("Error loop");
   while (1) {
     delay(100);
   }
 }
 
+#if ENABLE_PUBLISHER
+void timer_callback(rcl_timer_t * timer, int64_t last_call_time) {
+  RCLC_UNUSED(last_call_time);
+  if (timer != NULL) {
+    RCSOFTCHECK(rcl_publish(&publisher, &msg, NULL));
+    msg.data++;
+    Serial.println("Published message");
+  }
+}
+#endif
+
+#if ENABLE_SUBSCRIBER
 void twist_callback(const void *msg_in) {
   const geometry_msgs__msg__Twist *msg =
       (const geometry_msgs__msg__Twist *)msg_in;
-  ESP_LOGI(TAG, "Received: linear.x %f, angular.z %f", msg->linear.x,
-           msg->angular.z);
+  Serial.println("Received twist message");
+  Serial.print("Linear: ");
+  Serial.print(msg->linear.x);
+  Serial.print(", ");
+  Serial.print("Angular: ");
+  Serial.print(msg->angular.z);
+  Serial.println();
 }
+#endif
 
 void setup() {
   // Configure serial transport
   Serial.begin(115200);
+  Serial.println("Started");
 
   char ssid[] = SSID;
   char ssid_pw[] = SSID_PW;
-  IPAddress agent_ip(192, 168, 54, 2);
+  IPAddress agent_ip(192, 168, 0, 2);
   const uint16_t k_agent_port = 8888;
   set_microros_wifi_transports(ssid, ssid_pw, agent_ip, k_agent_port);
   delay(2000);
@@ -82,30 +105,39 @@ void setup() {
   // create node
   RCCHECK(rclc_node_init_default(&node, "micro_ros_platformio_node", "", &support));
 
+#if ENABLE_PUBLISHER
   // create publisher
-  // RCCHECK(rclc_publisher_init_default(
-  //   &publisher,
-  //   &node,
-  //   ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
-  //   "micro_ros_platformio_node_publisher"));
+  RCCHECK(rclc_publisher_init_default(
+    &publisher,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+    "micro_ros_platformio_node_publisher"));
 
-  // create timer,
-  // const unsigned int timer_timeout = 1000;
-  // RCCHECK(rclc_timer_init_default(
-  //   &timer,
-  //   &support,
-  //   RCL_MS_TO_NS(timer_timeout),
-  //   timer_callback));
+  // create timer.
+  const unsigned int timer_timeout = 1000;
+  RCCHECK(rclc_timer_init_default(
+    &timer,
+    &support,
+    RCL_MS_TO_NS(timer_timeout),
+    timer_callback));
+#endif
 
+#if ENABLE_SUBSCRIBER
+  // create subscriber.
   RCCHECK(rclc_subscription_init_best_effort(
       &subscriber_twist, &node,
       ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Twist), k_twist));
+#endif
 
   // create executor
-  RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
-  // RCCHECK(rclc_executor_add_timer(&executor, &timer));
+  RCCHECK(rclc_executor_init(&executor, &support.context, 2, &allocator));
+#if ENABLE_PUBLISHER
+  RCCHECK(rclc_executor_add_timer(&executor, &timer));
+#endif
+#if ENABLE_SUBSCRIBER
   RCCHECK(rclc_executor_add_subscription(
       &executor, &subscriber_twist, &twist_msg, &twist_callback, ON_NEW_DATA));
+#endif
 }
 
 void loop() {
